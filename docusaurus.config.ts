a2cd296e79ecec676bca86aa11cacea54a0a88cc
Igor <nik_101@live.co.uk>
2025-03-22 15:09:01 -0400

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,7 +20,7 @@ const config: Config = {
   organizationName: 'criticalflaw', // Usually your GitHub org/user name.
   projectName: 'criticalflaw.github.io', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
